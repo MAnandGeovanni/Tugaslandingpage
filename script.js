7f19a9237e003e4d8946f8c021c711b855c58610
MAnandGeovanni <119845105+MAnandGeovanni@users.noreply.github.com>
2023-09-22 22:22:05 +0700

    Tugaslandingpage

---
+++ script.js
@@ -0,0 +1,48 @@
+function handleGetFormData() {
+  const name = document.getElementById("name").value;
+  const city = document.getElementById("city").value;
+  const email = document.getElementById("email").value;
+  const zipCode = document.getElementById("zip-code").value;
+  const status = document.getElementById("status").checked;
+
+  return {
+    name,
+    city,
+    email,
+    zipCode,
+    status,
+  };
+}
+
+function isNumber(input) {
+  return !isNaN(input);
+}
+
+function checkboxIsChecked() {
+  return document.getElementById("status").checked;
+}
+
+function validateFormData(formData) {
+  return (
+    formData &&
+    formData.name.trim() !== "" &&
+    formData.city.trim() !== "" &&
+    formData.email.trim() !== "" &&
+    isNumber(formData.zipCode) &&
+    checkboxIsChecked()
+  );
+}
+
+function submit() {
+  const warningDiv = document.getElementById("warning");
+
+  if (!validateFormData(handleGetFormData())) {
+    warningDiv.innerHTML = "Periksa form anda sekali lagi";
+  } else {
+    warningDiv.innerHTML = "";
+  }
+}
+document.getElementById("submit-form").addEventListener("click", (event) => {
+  event.preventDefault();
+  submit();
+});
